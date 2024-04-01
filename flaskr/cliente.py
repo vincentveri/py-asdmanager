6a08c27ea45e711af557ebc8e74523df9d9d2e91
Vincent Veri <vincent.veri@gmail.com>
2024-04-01 17:40:12 +0200

    aggiornamento cliente

--- flaskr/cliente.py
+++ flaskr/cliente.py
@@ -3,6 +3,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
+from sqlalchemy import update as upd
+
 from flaskr import db
 from flaskr.auth import login, login_required
 from flaskr.models import Cliente
@@ -41,47 +43,35 @@ def create():
     return render_template('cliente/create.html')
 
 
-def get_cliente(id, check_author=True):
-    post = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    return post
-
-
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@bp.route('/<int:id>', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    post = get_cliente(id)
+    cliente = db.get_or_404(Cliente, id)
 
+    # Effettua refactor con
+    # https://docs.sqlalchemy.org/en/20/tutorial/orm_data_manipulation.html
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        cognome = request.form['cognome']
+        nome = request.form['nome']
+        codice_fiscale = request.form['codice_fiscale']
+
         error = None
 
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
+        if not cognome or not nome:
+            error = 'Lastname and name are required'
             flash(error)
-        else:
-            db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            flash("Messaggio salvato", "success")
-            return redirect(url_for('invoice.index'))
 
-    return render_template('invoice/update.html', post=post)
+        if error is None:
+
+            db.session.execute(
+                upd(Cliente),
+                [
+                    {"id": id, "cognome": cognome, "nome": nome, "codice_fiscale": codice_fiscale}
+                ]
+            )
+            db.session.commit()
+
+            flash("Cliente salvato", "success")
+            return redirect(url_for('cliente.index'))
+
+    return render_template('cliente/update.html', cliente=cliente)
